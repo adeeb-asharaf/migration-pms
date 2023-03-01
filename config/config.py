5deb5052d441e7b0105cd9c94386e70aac81a4e7
Adeeb Mohammed <adeeb.asharaf@tabist.com>
2023-03-02 06:49:31 +0900

    initial commit

---
+++ config/config.py
@@ -0,0 +1,13 @@
+class Config:
+    MASK_FIELD_FILE_NAME = "mask_field_names.csv"
+    IGNORE_TABLES_FILE_NAME = "ignore_tables.csv"
+    DB_END_POINT = "pmsproddb.cu5vbf0p8xw1.ap-northeast-1.rds.amazonaws.com"
+    DB_NAME = "pmsproddb"
+    DB_PORT_NUMBER = "5432"
+    DB_USER_NAME = "pmsprod"
+    DB_PASSWORD = ""
+    OUTPUT_FILE_NAME = "db_dump.sql"
+    CSV_PATH = "output/csv/"
+    SQL_PATH = "output/sql/"
+    NUMERIC_TYPE = 'NUMBER'
+    CHARACTER_TYPE = 'CHARACTER'
